b24d683658245eb69983fac4ded758453578ccaa
NHD39 <nguyenduc0392004@gmail.com>
2023-12-05 20:32:31 +0700

    first commit

---
+++ tool.mjs
@@ -0,0 +1,31 @@
+import express from 'express'
+import fs from 'fs'
+import puppeteer from 'puppeteer'
+let data = fs.readFileSync("./listPhone.doc", "utf-8")
+data = data.split(",")
+const tool = express()
+tool.get('/', async (req, res) => {
+    for (let i = 0; i < data.length; i++) {
+        if (i == data.length - 1) {
+            res.redirect('/done')
+        }
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1366, height: 768 })
+        await page.goto('https://hi88.tv/');
+        await page.type('#code-id', data[i]);
+        await page.waitForSelector('#submit-btn');
+        await page.click('#submit-btn');
+        await page.waitForSelector('#captcha');
+        const content = await page.$eval('#captcha', div => div.textContent);
+        await page.type('#captcha-input', content);
+        await browser.close()
+    }
+})
+tool.get('/done', (req, res) => {
+    res.send("done")
+})
+tool.listen(8000, () => {
+    console.log("tool is running on the port 8000")
+})
+
